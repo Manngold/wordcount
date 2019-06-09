dbeec9188f28da99ac66cfdab58478c818bac331
Manngold <jsw4820@gmail.com>
2019-06-09 17:10:43 +0900

    complete logic

--- wordcount/myapp/views.py
+++ wordcount/myapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def result(request):
+    text = request.GET['fulltext']
+    words = text.split()
+    wordStorage = {}
+
+    for word in words:
+        if word in wordStorage:
+            wordStorage[word]+=1
+        else:
+            wordStorage[word] = 1
+
+    return render(request, 'result.html', {'fulltext': text, 'total': len(words), 'dictionary': wordStorage.items()})
